ea22c4906de065296aaa22a5dc416047e853f396
Isuru Fernando <isuruf@gmail.com>
2020-12-18 16:30:10 -0800

    fix cross compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
+  (
+    mkdir -p native-build
+    pushd native-build
+
+    export CC=$CC_FOR_BUILD
+    export AR=($CC_FOR_BUILD -print-prog-name=ar)
+    export NM=($CC_FOR_BUILD -print-prog-name=nm)
+    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+    export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
+
+    # Unset them as we're ok with builds that are either slow or non-portable
+    unset CFLAGS
+    unset CPPFLAGS
+
+    cmake \
+      -GNinja \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      ..
+    ninja -j${CPU_COUNT}
+  )
+  CMAKE_ARGS="${CMAKE_ARGS} -DNATIVE_BUILD_DIR=$PWD/native-build"
+fi
+
 mkdir build
 cd build
 
